6b843c7934602269ecf6c6888ad36b4dc9b45d55
Sharyoo <kodagolden808@yahoo.com>
2025-04-16 13:34:32 -0400

    wasd

--- Regions/Andalusia/Andalusia.js
+++ Regions/Andalusia/Andalusia.js
@@ -1,9 +1,22 @@
 $("#Cadiz, #Malaga, #Granada, #Almeria, #Jaen, #Cordoba, #Sevilla, #Huelva, #Capital").hover(function() {
-    console.log(this.id);
+    
+    if (this.id == "Capital") {
+        $("#Sevilla").css('fill', 'white');
+    }
+    else if (this.id == "Sevilla") {
+        $("#Capital").css('fill', 'green');
+    }
+
     $(this).css('fill', 'green');
     var newElement = $('<div class="new-element">' + this.id + '</div>');
     $("body").append(newElement);
+
 }, function() {
+
+    if (this.id == "Sevilla") {
+        $("#Capital").css('fill', 'white');
+    }
+
     $(this).css('fill', 'white');
     $("body").find('.new-element').remove();
 })
